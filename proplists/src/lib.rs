900e94fd66e73c883512ba6173972d3633c11829
Tijl Vercaemer <1560340+Blandinium@users.noreply.github.com>
2026-03-15 21:38:05 +0000

    Added iterators and some other traits

--- proplists/src/lib.rs
+++ proplists/src/lib.rs
@@ -30,6 +30,25 @@ impl<T> Clone for List<T> {
     }
 }
 
+impl<T> Default for List<T> {
+    fn default() -> Self {
+        Self::empty()
+    }
+}
+
+impl<T: std::fmt::Debug> std::fmt::Debug for List<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_list().entries(self.iter()).finish()
+    }
+}
+
+impl<T: PartialEq> PartialEq for List<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.iter().eq(other.iter())
+    }
+}
+
+impl<T: Eq> Eq for List<T> {}
 
 impl<T> List<T> {
     /// Returns an empty list.
@@ -54,7 +73,7 @@ impl<T> List<T> {
     /// assert_eq!(list.hd(), Some(&42));
     /// ```
     pub fn new(data: T) -> Self {
-        Self(Arc::new(ListCell::Cons(data, Arc::new(ListCell::Nil))))
+        Self::cons(data, &Self::empty())
     }
     /// Returns a new list with `data` at the head, followed by the contents of `list`.
     ///
@@ -121,5 +140,110 @@ impl<T> List<T> {
             ListCell::Cons(_, _) => false,
             ListCell::Nil => true,
         }
+    }
+
+    /// Returns an iterator over the list.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use proplists::List;
+    /// let list = List::cons(1, &List::new(2));
+    /// let mut iter = list.iter();
+    /// assert_eq!(iter.next(), Some(&1));
+    /// assert_eq!(iter.next(), Some(&2));
+    /// assert_eq!(iter.next(), None);
+    /// ```
+    pub fn iter(&self) -> Iter<'_, T> {
+        Iter { next: &*self.0 }
+    }
+}
+
+/// An iterator over the elements of a `List`.
+///
+/// This struct is created by the `iter` method on `List` or by `into_iter` on `&List`.
+///
+/// # Examples
+///
+/// ```
+/// use proplists::List;
+/// let list = List::cons(1, &List::new(2));
+/// let mut iter = list.iter();
+/// assert_eq!(iter.next(), Some(&1));
+/// ```
+pub struct Iter<'a, T> {
+    next: &'a ListCell<T>
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.next {
+            ListCell::Nil => None,
+            ListCell::Cons(head, tail) => {
+                self.next = tail.as_ref();
+                Some(head)
+            }
+        }
+    }
+}
+
+impl<'a, T> IntoIterator for &'a List<T> {
+    type Item = &'a T;
+    type IntoIter = Iter<'a, T>;
+
+    /// Returns an iterator over the list.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use proplists::List;
+    /// let list = List::cons(1, &List::new(2));
+    /// for element in &list {
+    ///     println!("{}", element);
+    /// }
+    /// ```
+    fn into_iter(self) -> Self::IntoIter {
+        self.iter()
+    }
+}
+
+impl<T> std::iter::FromIterator<T> for List<T> {
+    /// Creates a `List` from an iterator.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use proplists::List;
+    /// let vec = vec![1, 2, 3];
+    /// let list: List<i32> = vec.into_iter().collect();
+    /// assert_eq!(list.hd(), Some(&1));
+    /// assert_eq!(list.tl().unwrap().hd(), Some(&2));
+    /// ```
+    fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
+        let items: Vec<T> = iter.into_iter().collect();
+        items.into_iter()
+            .rev()
+            .fold(List::empty(), |acc, item| List::cons(item, &acc))
+    }
+}
+
+impl<T: Clone> List<T> {
+    /// Returns a new list with the (cloned) elements in reverse order.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use proplists::List;
+    /// let list = List::cons(1, &List::new(2));
+    /// let reversed = list.reverse();
+    /// assert_eq!(reversed.hd(), Some(&2));
+    /// assert_eq!(reversed.tl().unwrap().hd(), Some(&1));
+    /// ```
+    pub fn reverse(&self) -> Self {
+        self.iter()
+            .cloned()
+            .fold(List::empty(), |acc, item| List::cons(item, &acc))
     }
 }
